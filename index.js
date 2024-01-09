313d8c8af86f97514de97995ddea7ed37a439e18
Pabolo13 <pablo2020b@gmail.com>
2024-01-08 21:38:40 -0300

    mudanças Html e css, adição Js

---
+++ index.js
@@ -0,0 +1,14 @@
+let name = 'King';
+let altura = '179';
+let idade = '20';
+let estuda = true;
+
+let pessoa = {
+    nome : 'Pablo',
+    idade : 20,
+    estuda : true
+};
+let chico = ['lindo','tesao','bonito','gostosao'];
+
+console.log(pessoa);
+console.log(chico);
